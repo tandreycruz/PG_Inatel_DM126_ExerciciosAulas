3180586cae9e3410608cb27f0a38c35e5a3332e4
user288577 <user288577@#13.104>
2025-12-06 15:47:35 -0500

    Primeiro commit do projeto do exercicio da aula 2

---
+++ ExercicioAula2/ExercicioAula2/ExercicioAula2App.swift
@@ -0,0 +1,17 @@
+//
+//  ExercicioAula2App.swift
+//  ExercicioAula2
+//
+//  Created by user288577 on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ExercicioAula2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
