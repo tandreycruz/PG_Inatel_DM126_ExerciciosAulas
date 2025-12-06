c6c3adb749f56fa2983b0ad727e3cf388b40309d
user288577 <user288577@#13.104>
2025-12-06 16:18:19 -0500

    Primeiras alteracoes login comum

--- ExercicioAula2/ExercicioAula2/ContentView.swift
+++ ExercicioAula2/ExercicioAula2/ContentView.swift
@@ -8,16 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var alertMessage: String = ""
+    @State private var showAlert: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            TextField(
+                "username", text: $username
+            ).frame(maxWidth: 150)
+                    
+            HStack {
+                SecureField(
+                    "password", text: $password
+                ).frame(maxWidth: 200)
+                .textContentType(.password)
+                        
+                Button(action: handleLogin) {
+                    Image(systemName: "greaterthan.square.fill")
+                }
+            }.frame(maxWidth: 150)
+        }.frame(maxWidth: 200)
+                
+        .alert(alertMessage, isPresented:
+            $showAlert){
+                Button("Ok", role: .cancel){}
+            }
     }
-}
+    
+    private func handleLogin(){
+        if username == "Admin" && password == "123" {
+            alertMessage = "Login feito com sucesso!"
+        }
+        else {
+            alertMessage = "Credenciais invalidas!"
+        }
+            
+        showAlert = true
+    }}
 
 #Preview {
     ContentView()
