c930b0da91313358c8029bbdfec7bcd2c5a4a30f
user288577 <user288577@#13.104>
2025-12-08 12:39:39 -0500

    Atualiza .gitignore com regras corretas

--- ExerciciosAulas3e4/App_BemEstar/App_BemEstar/NavigationBar/NavigationBar.swift
+++ ExerciciosAulas3e4/App_BemEstar/App_BemEstar/NavigationBar/NavigationBar.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct NavigationBar: View {
     @State private var nomeAtual: String = "Taibe Cruz"
-    @State private var nomes: [String] = ["Taibe Cruz", "Jose Cruz", "Ramon Cruz"]
+    @State private var nomes: [String] = ["Taibe Cruz", "Carlitos Tevez", "Romarinho"]
     @State private var showAlert: Bool = false
     @State private var nomeNovo: String = ""
     
@@ -29,8 +29,30 @@ struct NavigationBar: View {
                     .fontWeight(.bold)
             }
             
-            Button(action: {}){
-                Image(systemName: "person.badge.plus.fill")
+            Button(action: {showAlert = true}){
+                Image(systemName: "plus.circle.fill")
+            .font(.title3)
+            .foregroundStyle(.gray)
+            }.sheet(isPresented: $showAlert){
+                VStack{
+                    Text("Adicionar novo nome").font(.headline)
+                    TextField("Digite o nome", text: $nomeNovo).textFieldStyle(RoundedBorderTextFieldStyle())
+                    HStack {
+                        Button("Cancelar") {
+                            showAlert = false
+                            nomeNovo = ""
+                        }
+                        .foregroundStyle(.red)
+                        Button("Adicionar") {
+                            if !nomeNovo.isEmpty {
+                                nomes.append(nomeNovo)
+                                nomeAtual = nomeNovo
+                                nomeNovo = ""
+                                showAlert = false
+                            }
+                        }.foregroundStyle(.blue)
+                    }
+                }.presentationDetents([.fraction(0.2)]).padding()
             }
         }
     }
