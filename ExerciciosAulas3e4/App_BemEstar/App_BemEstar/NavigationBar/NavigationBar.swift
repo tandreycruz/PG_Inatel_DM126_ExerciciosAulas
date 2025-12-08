b584dab59a1647ee61fa59343d04e2e99b86bc33
user288577 <user288577@#13.104>
2025-12-08 10:50:56 -0500

    Troca de nome no NavigationBar

--- ExerciciosAulas3e4/App_BemEstar/App_BemEstar/NavigationBar/NavigationBar.swift
+++ ExerciciosAulas3e4/App_BemEstar/App_BemEstar/NavigationBar/NavigationBar.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct NavigationBar: View {
+    @State private var nomeAtual: String = "Taibe Cruz"
+    @State private var nomes: [String] = ["Taibe Cruz", "Jose Cruz", "Ramon Cruz"]
+    @State private var showAlert: Bool = false
+    @State private var nomeNovo: String = ""
+    
     var body: some View {
         HStack{
-            Button("Taibe Cruz"){}
-                .font(.headline)
-                .fontWeight(.bold)
+            Menu{
+                ForEach(nomes, id: \.self){
+                    nome in
+                    Button(nome)
+                    {
+                        nomeAtual = nome
+                    }
+                }
+            }label: {
+                Text(nomeAtual)
+                    .font(.headline)
+                    .fontWeight(.bold)
+            }
             
             Button(action: {}){
                 Image(systemName: "person.badge.plus.fill")
